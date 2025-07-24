761ef059c6c565d0b879df3cf1bf2694047915fd
quinnsm97 <199311766+quinnsm97@users.noreply.github.com>
2025-07-24 20:59:35 +1000

    added teacher relation to course

--- models/teacher.py
+++ models/teacher.py
@@ -1,10 +1,12 @@
 from init import db
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+from marshmallow import fields 
+from marshmallow.validate import Length, And, Regexp
 
 
 class Teacher(db.Model):
     # Name of the table
-    __tablename__ = "teachers"
+    # __tablename__ = "teachers"
 
     # Attributes
     id = db.Column(db.Integer,primary_key=True)
@@ -12,7 +14,10 @@ class Teacher(db.Model):
     department = db.Column(db.String(100), nullable=False)
     address = db.Column(db.String(150))
 
+    courses = db.relationship("Course", back_populates="teacher")
+
 class TeacherSchema(SQLAlchemyAutoSchema):
+    courses = fields.List(fields.Nested("CourseSchema", exclude=("teacher",)))
     class Meta:
         model = Teacher
         load_instance = True
